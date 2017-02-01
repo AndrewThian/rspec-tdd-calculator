b383bbc612774ecc9200d74bcd93d5b7ea3690c1
cavacado <ngzhaoloong@gmail.com>
2017-02-01 13:20:27 +0800

    2nd

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,10 +1,15 @@
 class Calculator
   # read_only
-  attr_reader :result
+  # attr_reader :result
+  # see if tests pass even if i put accessor
+  attr_accessor :result
 
   # your class goes here
   def initialize (cal_val=1)
     @result = cal_val
   end
 
+  def reset(x)
+    @result = x
+  end
 end
