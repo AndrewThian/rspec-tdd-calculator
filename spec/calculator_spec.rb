22e5218f1f8d7121e3fd15571f29cff0970e7286
cavacado <ngzhaoloong@gmail.com>
2017-02-01 12:54:41 +0800

    first test

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,5 +1,5 @@
 require_relative 'spec_helper'
-require_relative '../lib/item'
+require_relative '../lib/calculator'
 
 describe Calculator do
   before(:context) do
@@ -7,4 +7,12 @@ describe Calculator do
   end
 
   # Tests go here
+  describe "Initialization" do
+    it "is an instance of the Calculator class" do
+      expect(@my_calculator).to be_instance_of(Calculator)
+    end
+    it "should assign 1 to be the starting result of the new instance" do
+      expect(@my_calculator.result).to eq(1)
+    end
+  end
 end
